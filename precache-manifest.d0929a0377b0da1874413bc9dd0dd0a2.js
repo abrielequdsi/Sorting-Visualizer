f0475b63d5aadb5a53b8a79ffadf45f962bc9e23
Abriele Qudsi <abrielebromozentia@gmail.com>
2020-07-21 23:44:46 +0700

    Updates

---
+++ precache-manifest.d0929a0377b0da1874413bc9dd0dd0a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acbe2dae2e067b736421303b823899a4",
+    "url": "/Sorting-Visualizer/index.html"
+  },
+  {
+    "revision": "f4282b1b505d10a3ad7e",
+    "url": "/Sorting-Visualizer/static/css/main.7f92a5ad.chunk.css"
+  },
+  {
+    "revision": "1776402146f3f10cc5b2",
+    "url": "/Sorting-Visualizer/static/js/2.b54e0049.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Sorting-Visualizer/static/js/2.b54e0049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4282b1b505d10a3ad7e",
+    "url": "/Sorting-Visualizer/static/js/main.1457696e.chunk.js"
+  },
+  {
+    "revision": "f4855d1cc18174031e07",
+    "url": "/Sorting-Visualizer/static/js/runtime-main.41773b78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sorting-Visualizer/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sorting-visualizer/precache-manifest.514eeda02f490d1be42fa5cf3cec3d69.js"
+  "/Sorting-Visualizer/precache-manifest.d0929a0377b0da1874413bc9dd0dd0a2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sorting-visualizer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sorting-Visualizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
